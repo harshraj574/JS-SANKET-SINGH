19e0375cf14722885256ba1b58c0861ca7989474
harshraj574 <harshraj574@gmail.com>
2024-06-29 05:26:47 +0000

    js-conditionals

---
+++ JS-CONDITIONALS-PROBLEMS/oddeven.js
@@ -0,0 +1,10 @@
+function OddOrEven(x){
+    if(x%2 == 0){
+        return "Even"
+    }
+    else{
+        return "Odd"
+    }
+}
+let ans =   OddOrEven(5);
+console.log(ans);
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A simple HTML/JS/CSS starter template
+JS-PRACTICE-PROBLEMS
